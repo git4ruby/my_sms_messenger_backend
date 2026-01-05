9b94bb7abe1ff11461d247d18a5a972e93a59da7
Mohit Chandana <chandanamohit@gmail.com>
2026-01-05 14:36:41 -0500

    Cleanup unwanted ssl settings

--- config/initializers/mongoid.rb
+++ config/initializers/mongoid.rb
@@ -1,33 +1,4 @@
 # rubocop:todo all
-
-# Custom SSL context for MongoDB Atlas connection
-if Rails.env.production?
-  require 'openssl'
-  require 'mongo'
-
-  # Patch OpenSSL defaults globally for MongoDB connections
-  OpenSSL::SSL::SSLContext::DEFAULT_PARAMS[:verify_mode] = OpenSSL::SSL::VERIFY_NONE
-  OpenSSL::SSL::SSLContext::DEFAULT_PARAMS[:min_version] = OpenSSL::SSL::TLS1_2_VERSION
-
-  # Patch the Mongo driver's SSL context creation
-  Mongo::Socket::SSL.class_eval do
-    private
-
-    def create_context(options = {})
-      context = OpenSSL::SSL::SSLContext.new
-      context.verify_mode = OpenSSL::SSL::VERIFY_NONE
-      context.min_version = OpenSSL::SSL::TLS1_2_VERSION
-      context.max_version = OpenSSL::SSL::TLS1_3_VERSION
-      # Disable all SSL/TLS compression to avoid CRIME attacks
-      context.options |= OpenSSL::SSL::OP_NO_COMPRESSION if defined?(OpenSSL::SSL::OP_NO_COMPRESSION)
-      context
-    end
-  end
-
-  Rails.logger.info "OpenSSL Version: #{OpenSSL::OPENSSL_VERSION}"
-  Rails.logger.info "OpenSSL Library Version: #{OpenSSL::OPENSSL_LIBRARY_VERSION}"
-end
-
 Mongoid.configure do
   target_version = "9.0"
 
