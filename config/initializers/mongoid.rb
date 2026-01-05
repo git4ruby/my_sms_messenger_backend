67f34f5b93edf08fb842ddbdd5faaf783fd5a611
Mohit Chandana <chandanamohit@gmail.com>
2026-01-05 13:07:50 -0500

    Add custom SSL context for MongoDB Atlas connection

--- config/initializers/mongoid.rb
+++ config/initializers/mongoid.rb
@@ -1,4 +1,38 @@
 # rubocop:todo all
+
+# Custom SSL context for MongoDB Atlas connection
+if Rails.env.production?
+  require 'openssl'
+  require 'mongo'
+
+  # Create a custom SSL context that works with MongoDB Atlas
+  ssl_context = OpenSSL::SSL::SSLContext.new
+  ssl_context.verify_mode = OpenSSL::SSL::VERIFY_NONE
+  ssl_context.set_params(
+    verify_mode: OpenSSL::SSL::VERIFY_NONE,
+    min_version: OpenSSL::SSL::TLS1_2_VERSION
+  )
+
+  # Monkey patch the Mongo client to use our custom SSL context
+  module Mongo
+    class Socket
+      class SSL < Socket
+        private
+
+        def create_context
+          context = OpenSSL::SSL::SSLContext.new
+          context.verify_mode = OpenSSL::SSL::VERIFY_NONE
+          context.set_params(
+            verify_mode: OpenSSL::SSL::VERIFY_NONE,
+            min_version: OpenSSL::SSL::TLS1_2_VERSION
+          )
+          context
+        end
+      end
+    end
+  end
+end
+
 Mongoid.configure do
   target_version = "9.0"
 
