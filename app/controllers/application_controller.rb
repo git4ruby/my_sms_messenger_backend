6d490ad6a10274429f7bab07935c1bad44baeeae
Mohit Chandana <chandanamohit@gmail.com>
2026-01-05 23:49:06 -0500

    Add custom authenticate_user methods to override devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::API
+  attr_reader :current_user
+
+  protected
+
+  def authenticate_user!
+    auth_header = request.headers['Authorization']
+    token = auth_header&.split(' ')&.last
+
+    return render_unauthorized unless token
+
+    payload = JwtService.decode(token)
+    @current_user = User.find(payload['user_id'])
+  rescue JWT::DecodeError, Mongoid::Errors::DocumentNotFound
+    render_unauthorized
+  end
+
+  def render_unauthorized
+    render json: {error: 'Unauthorized' }, status: :unauthorized
+  end
 end
