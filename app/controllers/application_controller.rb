2039b0556413a5a1c3faf995ebeb25b1e5800f4d
Mohit Chandana <chandanamohit@gmail.com>
2026-01-06 00:53:42 -0500

    Configure warden to use JWT authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,15 +7,4 @@ class ApplicationController < ActionController::API
     devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password, :password_confirmation])
     devise_parameter_sanitizer.permit(:sign_in, keys: [:email, :password])
   end
-
-  def authenticate_user!(opts = {})
-    token = request.headers['Authorization']&.split(' ')&.last
-    Rails.logger.info "JWT Token received: #{token&.slice(0, 20)}..."
-    Rails.logger.info "JWT Secret configured: #{Devise.jwt.secret.present?}"
-
-    super
-  rescue => e
-    Rails.logger.error "JWT Auth failed: #{e.class} - #{e.message}"
-    render json: { error: 'Unauthorized', details: e.message }, status: :unauthorized
-  end
 end
